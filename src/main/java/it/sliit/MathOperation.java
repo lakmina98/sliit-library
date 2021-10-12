1cb62425379bf5b0d1a4660980ff5169ecb772fe
lakmina98 <it18184686@my.sliit.lk>
2021-10-12 10:38:07 +0530

    multiply function

--- src/main/java/it/sliit/MathOperation.java
+++ src/main/java/it/sliit/MathOperation.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 
 public class MathOperation {
 
+    
     public static double add(double... operands)
     {
         return Arrays.stream(operands).reduce(0d, Double::sum);
@@ -13,6 +14,10 @@ public class MathOperation {
     public static double subtract(double operand1, double operand2)
     {
         return operand1 - operand2;
+    }
+
+    public static int multiply(a, b){
+        return a*b;
     }
 
 }
